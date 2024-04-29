517741edc17bdf359494609ddc410acb0d50987c
Chan Myae Kyaw <chanmyaekyawmay@gmail.com>
2024-04-29 18:42:51 +0800

    Create Footer.jsx

---
+++ SENSE/src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='my-2.5 mx-10 flex items-center justify-between border-t border-solid border-gray-600 py-4'>
+      <p>@ 2024 SENSE. All rights reserved.</p>
+      <ul>
+        <li className='list-none inline-block ml-5'>Terms of Services</li>
+        <li className='list-none inline-block ml-5'>Privacy Policy</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
