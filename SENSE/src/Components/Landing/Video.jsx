a8b3204e7e2c96490076d41e444d250a6ebf1d52
Chan Myae Kyaw <chanmyaekyawmay@gmail.com>
2024-07-11 22:14:53 +0800

    Update Video with a youTube link
    Haven't linked with API yet

--- SENSE/src/Components/Landing/Video.jsx
+++ SENSE/src/Components/Landing/Video.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import videoSrc from '../../assets/demo.mp4';
 
 const Video = () => {
   return (
     <div name='video' className="flex flex-col items-center mt-10">
       <h2 className="text-black text-4xl text-center m-20 font-semibold">Demo Video</h2>
-      <video controls className="w-3/4 m-5">
-        <source src={videoSrc} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <div className="w-3/4 m-5">
+        <iframe
+          width="560"
+          height="500"
+          src="https://www.youtube.com/embed/_svOdVvX5LA?si=dRb8NMFLf8f6_crk"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+          className="w-full h-74 sm:h-96"
+        ></iframe>
+      </div>
     </div>
   );
 }
