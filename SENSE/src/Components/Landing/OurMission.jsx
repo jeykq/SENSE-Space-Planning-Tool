dbe5efc8108187d8121dbb80167d7875da985617
Chan Myae Kyaw <chanmyaekyawmay@gmail.com>
2024-07-13 19:38:32 +0800

    Finish Main Landing Page Fetch and show updated our mission paragraph

--- SENSE/src/Components/Landing/OurMission.jsx
+++ SENSE/src/Components/Landing/OurMission.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import LandingPageAPIUtils from '../SystemAdmin/LandingPageAPIUtils'; 
 
 const OurMission = () => {
+  const { missionParagraph, fetchLandingPageData } = LandingPageAPIUtils({
+    onUpdateSuccess: () => {}, // Define success callback if needed
+    onError: (error) => {
+      console.error('Error fetching data for OurMission component:', error);
+    }
+  });
+
+  useEffect(() => {
+    // Fetch initial data for OurMission component
+    fetchLandingPageData();
+  }, []);
+
   return (
     <div name='ourMission' className='text-black text-center mx-auto max-w-screen-lg p-6'>
       <h1 className='text-4xl font-semibold mb-10 text-blue-700'>Our Mission</h1>
       <p className='text-lg text-justify max-w-[800px] mt-10 md:mb-10 leading-relaxed mx-auto'>
-        At SENSE Spaces, our mission is to revolutionize the way you create and experience multi-sensory environments. We are dedicated to providing an accessible, user-friendly platform that empowers everyone—from therapists and educators to designers and hobbyists—to craft spaces that inspire, heal, and educate. By integrating cutting-edge technology with creative freedom, we aim to transform ordinary surroundings into extraordinary experiences, fostering well-being, learning, and connection in every project we support.
+        {missionParagraph}
       </p>
     </div>
   );
