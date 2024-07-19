7f21a967b5e21b5eb058aa7aac9f23bf9eca57a0
Kym Thang <kymthang@gmail.com>
2024-07-19 12:45:24 +0800

    added fu searchbar with filter for room design

---
+++ SENSE/src/Components/FreeUser/FU_SearchBar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const FU_SearchBar = ({ handleSearch }) => {
+  const [searchType, setSearchType] = useState("name");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleTypeChange = (e) => {
+    setSearchType(e.target.value);
+    handleSearch("", e.target.value); // Reset search query on type change
+  };
+
+  const handleSearchChange = (e) => {
+    handleSearch(e.target.value, searchType);
+  };
+
+  return (
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <form onSubmit={handleSubmit} style={{ display: "flex", alignItems: "center" }}>
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={handleSearchChange}
+          style={{ borderRadius: "5px", padding: "8px 5px", marginRight: "5px", width: "250px" }}
+        />
+        <label style={{ marginRight: "20px", color: "black", padding: "20px 10px", borderRadius: "2px" }}>Filter by:</label>
+        <select onChange={handleTypeChange} value={searchType} style={{ borderRadius: "2px", padding: "5px 5px", marginRight: "5px" }}>
+          <option value="name">Name</option>
+          <option value="category">Category</option>
+        </select>
+        <button type="submit" style={{ border: "none", borderRadius: "40px", background: "white", cursor: "pointer", padding: "5px 10px" }}>
+          <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+          </svg>
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default FU_SearchBar;
--- SENSE/src/Components/FreeUser/FreeUserHomepage.jsx
+++ SENSE/src/Components/FreeUser/FreeUserHomepage.jsx
@@ -4,24 +4,19 @@ import Swiper from "swiper";
 import Navbar from "./Navbar";
 import Footer from "../Landing/Footer";
 import axios from "axios";
-import { FaSearch } from "react-icons/fa";
 import { getHeaders } from '../../../apiUtils';
 import "./FreeUserHomepage.css";
+import FU_SearchBar from "./FU_SearchBar";
 
 const FreeUserHomepage = () => {
   const navigate = useNavigate();
   const swiperContainer1 = useRef(null);
   const swiperContainer2 = useRef(null);
-  const dropdownRef = useRef(null);
-  const [showDropdown, setShowDropdown] = useState(false);
-  const [dropdownPosition, setDropdownPosition] = useState({ x: 0, y: 0 });
-  const [deleteTemplateId, setDeleteTemplateId] = useState(null);
-  const [deleteTemplateURL, setDeleteTemplateURL] = useState(null);
   const [roomTypes, setRoomTypes] = useState([]);
   const [templateNames, setTemplateNames] = useState([]);
   const [error, setError] = useState(null);
-  const [activeSearchField, setActiveSearchField] = useState(null);
   const [searchQuery, setSearchQuery] = useState('');
+  const [searchType, setSearchType] = useState('name');
   const [refreshTemplates, setRefreshTemplates] = useState(false);
 
   useEffect(() => {
@@ -95,73 +90,11 @@ const FreeUserHomepage = () => {
         },
       });
     }
+  }, [templateNames]);
 
-    const handleScroll = () => {
-      if (showDropdown) {
-        const rect = swiperContainer1.current.getBoundingClientRect();
-        setDropdownPosition({ x: rect.left + window.scrollX, y: rect.bottom + window.scrollY });
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [showDropdown, roomTypes]);
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setShowDropdown(false);
-      }
-    };
-
-    document.addEventListener('click', handleClickOutside);
-
-    return () => {
-      document.removeEventListener('click', handleClickOutside);
-    };
-  }, []);
-
-  const toggleDropdown = (event, templateId, url) => {
-    event.stopPropagation();
-    setShowDropdown(!showDropdown);
-    const rect = event.target.getBoundingClientRect();
-    setDropdownPosition({ x: rect.left + window.scrollX, y: rect.bottom + window.scrollY });
-  };
-
-  const viewTemplate = (template) => {
-    const { 
-      name: templateName, 
-      room_type_id: roomType, 
-      dimension: { 
-        length: roomLength, 
-        width: roomWidth, 
-        height: roomHeight 
-      }, 
-      room_layout: { 
-        room_layout: roomLayoutUrl 
-      } } = template;
-
-    navigate('/FU_Room3D', {
-      state: {
-        templateName,
-        roomType,
-        roomLength: parseFloat(roomLength),
-        roomWidth: parseFloat(roomWidth),
-        roomHeight: parseFloat(roomHeight),
-        roomLayoutUrl
-      }
-    });
-  };
-
-  const toggleSearchField = (field) => {
-    setActiveSearchField(activeSearchField === field ? null : field);
-  };
-
-  const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
+  const handleSearch = (query, type) => {
+    setSearchQuery(query);
+    setSearchType(type);
   };
 
   const getRoomTypeName = (roomTypeId) => {
@@ -173,37 +106,31 @@ const FreeUserHomepage = () => {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
   };
 
+  const filteredTemplates = templateNames.filter(template => {
+    if (searchType === 'name') {
+      return template.name.toLowerCase().includes(searchQuery.toLowerCase());
+    } else if (searchType === 'category') {
+      const roomTypeName = getRoomTypeName(template.room_type_id).toLowerCase();
+      return roomTypeName.includes(searchQuery.toLowerCase());
+    }
+    return false;
+  });
+
   return (
     <div>
-      <div>
-        <Navbar />
-      </div>
+      <Navbar handleSearch={handleSearch} />
 
       <div style={{ paddingTop: "30px", paddingLeft: "20px", fontSize: "25px", fontWeight: "500", display: 'flex', alignItems: 'center' }}>
         <div className={"mt-20 ml-5"}>
           <p style={{ marginRight: '10px' }}>Room Templates</p>
         </div>
-        <div className={"mt-20 ml-10"} style={{ border: '1px solid black', borderRadius: '20px', padding: '10px 40px', cursor: 'pointer', display: 'flex', alignItems: 'center' }} onClick={() => toggleSearchField('roomTemplates')}>
-          <FaSearch />
-        </div>
-        {activeSearchField === 'roomTemplates' && (
-          <div style={{ marginLeft: '10px', display: 'flex', alignItems: 'center' }}>
-            <input
-              type="text"
-              value={searchQuery}
-              onChange={handleSearchChange}
-              placeholder="Search..."
-              style={{ padding: '10px', borderRadius: '20px', border: '1px solid black', width: '300px' }}
-            />
-          </div>
-        )}
       </div>
 
       <div style={{ paddingTop: "20px", paddingBottom: "0px", paddingLeft: "30px" }} className="justify-center">
         <div className="flex items-center" style={{ width: "96%" }}>
           <div ref={swiperContainer1} className="swiper-container" style={{ paddingRight: "40px", paddingBottom: "50px", width: "100%", height: "350px", overflow: "hidden" }}>
             <div className="swiper-wrapper">
-              {templateNames.map((template) => (
+              {filteredTemplates.map((template) => (
                 <div key={template.id} className="swiper-slide" style={{ position: 'relative', cursor: 'pointer' }} onClick={() => viewTemplate(template)}>
                   <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: '#D1D5DB', borderRadius: '20px', padding: '20px' }}>
                     <div className="bg-gray-300" style={{ borderRadius: '20px', height: '200px', marginBottom: '10px' }}></div>
@@ -224,31 +151,20 @@ const FreeUserHomepage = () => {
 
       <hr style={{ border: "1px solid black" }} />
 
-      <div style={{ paddingLeft: "20px", fontSize: "25px", fontWeight: "500", display: 'flex', alignItems: 'center' }}>
+      <div style={{ paddingLeft: "20px", fontWeight: "500", display: 'flex', alignItems: 'center' }}>
         <div className={"mt-5 ml-5"}>
-          <p style={{ marginRight: '10px' }}>Recent Designs</p>
+          <p style={{ marginRight: '10px', fontSize: "25px" }}>Recent Designs</p>
         </div>
-        <div className={"mt-5 ml-10"} style={{ border: '1px solid black', borderRadius: '20px', padding: '10px 40px', cursor: 'pointer', display: 'flex', alignItems: 'center' }} onClick={() => toggleSearchField('recentDesigns')}>
-          <FaSearch />
+        <div style={{ fontSize: "20px", marginLeft: '30px', display: 'flex', alignItems: 'center', marginTop: '15px' }}>
+          <FU_SearchBar handleSearch={handleSearch} />
         </div>
-        {activeSearchField === 'recentDesigns' && (
-          <div style={{ marginLeft: '10px', display: 'flex', alignItems: 'center' }}>
-            <input
-              type="text"
-              value={searchQuery}
-              onChange={handleSearchChange}
-              placeholder="Search..."
-              style={{ padding: '10px', borderRadius: '20px', border: '1px solid black', width: '300px' }}
-            />
-          </div>
-        )}
       </div>
 
       <div style={{ paddingTop: "20px", paddingBottom: "0px", paddingLeft: "30px" }} className="justify-center">
         <div className="flex items-center" style={{ width: "96%" }}>
           <div ref={swiperContainer2} className="swiper-container" style={{ paddingRight: "40px", paddingBottom: "50px", width: "100%", height: "350px", overflow: "hidden" }}>
             <div className="swiper-wrapper">
-              {templateNames.map((template) => (
+              {filteredTemplates.map((template) => (
                 <div key={template.id} className="swiper-slide" style={{ position: 'relative', cursor: 'pointer' }} onClick={() => viewTemplate(template)}>
                   <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: '#D1D5DB', borderRadius: '20px', padding: '20px' }}>
                     <div className="bg-gray-300" style={{ borderRadius: '20px', height: '200px', marginBottom: '10px' }}></div>
@@ -267,9 +183,7 @@ const FreeUserHomepage = () => {
         </div>
       </div>
 
-      <div>
-        <Footer />
-      </div>
+      <Footer />
     </div>
   );
 };
