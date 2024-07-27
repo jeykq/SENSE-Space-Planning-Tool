5afb04e8edb984fee5c97edb63ce6247a40c0a1d
Kym Thang <kymthang@gmail.com>
2024-07-27 20:31:28 +0800

    updated navbar freeuser

--- SENSE/src/Components/FreeUser/Navbar.jsx
+++ SENSE/src/Components/FreeUser/Navbar.jsx
@@ -67,6 +67,8 @@ const Navbar = () => {
     navigate('/FU_CreateRoom');
   };
 
+
+
   return (
     <nav className={`navbar bg-black text-white w-full py-1 px-4 fixed top-0 left-0 flex items-center justify-between z-10 ${sticky ? 'bg-black duration-75' : ''}`}>
       <img src={logo} alt="Logo" className='logo w-20 mx-2' />
@@ -77,12 +79,8 @@ const Navbar = () => {
       </button>
       <div className={`h-screen w-52 lg:w-auto lg:max-h-20 bg-black bg-opacity-100 lg:bg-opacity-100 translate-y-[375px] lg:translate-y-0 ${sidebarOpen ? 'translate-x-4 sticky right-0 top-0' : 'translate-x-full lg:translate-x-0 opacity-0 lg:opacity-100'} duration-100 lg:translate-x-0 px-4`}>
         <ul className="flex flex-col lg:flex-row mb-4 mt-20 lg:mt-1">
-          <li className="py-4 px-2 my-1.5 mx-2 lg:mx-5 text-base text-nowrap">
-            <Link onClick={() => setSidebarOpen(false)} to='Recent Designs' smooth={true} offset={0} duration={500}>Recent Designs</Link>
-          </li>
-          <li className="py-4 px-2 my-1.5 mx-2 lg:mx-5 text-base text-nowrap">
-            <Link onClick={() => setSidebarOpen(false)} to='Favourited' smooth={true} offset={0} duration={500}>Favourited</Link>
-          </li>
+          
+      
           <li className="flex items-center px-2 ml-1 my-1.5 mx-1 lg:mx-1 text base text-nowrap">
             <button className="bg-white hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
               style={{ borderRadius: "6px", backgroundColor: 'rgb(124 58 237)' }}
