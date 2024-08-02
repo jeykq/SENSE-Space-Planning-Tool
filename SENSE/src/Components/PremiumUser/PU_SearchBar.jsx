90de7a390f20e4de3c078bc0564a36a03ce62d3b
yixuanchua <166963686+yixuanchua@users.noreply.github.com>
2024-08-02 23:05:52 +0800

    Added dark mode for search bar component and commented out unused search button

--- SENSE/src/Components/PremiumUser/PU_SearchBar.jsx
+++ SENSE/src/Components/PremiumUser/PU_SearchBar.jsx
@@ -23,19 +23,19 @@ const PU_SearchBar = ({ handleSearch }) => {
           type="text"
           placeholder="Search..."
           onChange={handleSearchChange}
-          className="border"
+          className="border text-black"
           style={{ borderRadius: "5px", padding: "8px 5px", marginRight: "5px", width: "250px" }}
         />
-        <label style={{ marginRight: "20px", color: "black", padding: "20px 10px", borderRadius: "2px" }}>Filter by:</label>
+        <label style={{ marginRight: "20px", padding: "20px 10px", borderRadius: "2px" }} className="text-black dark:text-white">Filter by:</label>
         <select className="border rounded text-black" onChange={handleTypeChange} value={searchType} style={{padding: "5px 5px", marginRight: "5px" }}>
           <option value="name">Name</option>
           <option value="category">Category</option>
         </select>
-        <button type="submit" style={{ border: "none", borderRadius: "40px", background: "white", cursor: "pointer", padding: "5px 10px" }}>
+        {/* <button type="submit" style={{ border: "none", borderRadius: "40px", background: "white", cursor: "pointer", padding: "5px 10px" }}>
           <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
           </svg>
-        </button>
+        </button> */}
       </form>
     </div>
   );
