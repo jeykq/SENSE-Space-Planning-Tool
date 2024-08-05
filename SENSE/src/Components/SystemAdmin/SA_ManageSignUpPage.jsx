d0a6f092bc84d3aeba064506b57c00f1cd0ac22c
Chan Myae Kyaw <chanmyaekyawmay@gmail.com>
2024-08-05 22:49:39 +0800

    Obj Cat/Room Type/Sign up/Tags: Update Navbar to Topbar

--- SENSE/src/Components/SystemAdmin/SA_ManageSignUpPage.jsx
+++ SENSE/src/Components/SystemAdmin/SA_ManageSignUpPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Topbar from '../BusinessUser/Topbar';
+import Topbar from '../FreeUser/Topbar';
 import axios from 'axios';
 import { getHeaders } from '../../../apiUtils'; // Import the getHeaders function
 import AlertPopup from '../UI/AlertPopup'; // Assuming this is correctly imported
--- SENSE/src/Components/SystemAdmin/SA_ManageTags.jsx
+++ SENSE/src/Components/SystemAdmin/SA_ManageTags.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Topbar from '../BusinessUser/Topbar';
+import Topbar from '../FreeUser/Topbar';
 import axios from 'axios';
 import { getHeaders } from '../../../apiUtils'; 
 import AlertPopup from '../UI/AlertPopup'; 
