dd3ac7e68950eda4a6f09b3eb0e1a73269a2cea5
Jey <joieyeo@gmail.com>
2024-08-16 02:03:41 +0800

    Fixed Update Landing Page plan section issue

--- SENSE/src/Components/SystemAdmin/LandingPlan.jsx
+++ SENSE/src/Components/SystemAdmin/LandingPlan.jsx
@@ -119,10 +119,9 @@ const LandingPlan = () => {
 
   return (
     <div name='plan' className="flex flex-col items-center mt-10">
-      <div className={`flex flex-row space-x-4 ${editMode ? 'h-auto w-auto' : ''}`}>
+      <div className="flex flex-row space-x-4">
         {/* Free Account */}
-        <div className={`w-96 bg-white rounded-lg p-6 ${editMode ? 'h-auto' : 'h-80'}`} style={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)' }}>
-
+        <div className="w-96 bg-white rounded-lg p-6 h-auto" style={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)' }}>
           <div className="text-center mb-6">
             <div className="bg-blue-500 text-white rounded-md py-2 px-4 inline-block">Free Account</div>
           </div>
@@ -135,7 +134,7 @@ const LandingPlan = () => {
         </div>
 
         {/* Premium */}
-        <div className={`w-96 bg-white rounded-lg p-6 ${editMode ? 'h-auto' : 'h-80'}`} style={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)' }}>
+        <div className="w-96 bg-white rounded-lg p-6 h-auto" style={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)' }}>
           <div className="text-center mb-6">
             <div className="bg-orange-500 text-white rounded-md py-2 px-4 inline-block">Premium</div>
           </div>
